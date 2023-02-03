7f6b156935806eee2da80b495cf878b566e60a73
Bryan <bryanlu.my@gmail.com>
2023-02-03 17:58:08 +0800

    what is react arfghhhhhghsgsfa

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+
+		// <div className="top-container">
+		// 	<div className="top-title"><span style="color: white;">EXTRA</span><span style="color: #0B963A;">VEGAN</span><span
+		// 		style="color: black;">ZA</span>
+		// 	</div>
+		// </div>
+
+		<div>
+			<div className="top-container">
+				<div className="top-title"><span style={{ color: "white" }}>EXTRA</span><span style={{ color: "#0B963A" }}>VEGAN</span><span
+					style={{ color: "black" }}>ZA</span>
+				</div>
+			</div>
+
+			<div className="middle-container">
+
+			</div>
+
+			<div class="bottom-container">
+			</div>
+		</div>
+	);
 }
 
 export default App;
