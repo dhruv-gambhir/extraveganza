d5d21773c7bd17c391364eb1482ad558dfa3d56f
Hasterwfsc <90106147+Hasterwfsc@users.noreply.github.com>
2023-03-12 16:50:20 +0800

    Google Map implementation

--- app/src/scripts/MapPage/MapPage.js
+++ app/src/scripts/MapPage/MapPage.js
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import PageTemplate from "../PageTemplate";
 import Search from "../Utils/Search";
+import SimpleMap from "./SimpleMap";
 
 export default class MapPage extends PageTemplate {
     render() {
@@ -8,9 +9,7 @@ export default class MapPage extends PageTemplate {
             <Fragment>
                 <div className="main-content-container">
                     <Search />
-                    {/*<Map/>*/}
-                    {/*Hey Google, take me to some vegatarian cafe*/}
-
+                    <SimpleMap/>
                 </div>
             </Fragment>
         );
