d40b655214c883e23121b94ce3b9acf993bb114c
Enicul <93476156+Enicul@users.noreply.github.com>
2023-03-13 21:57:50 +0800

    add in Comment

--- SC2006-Backend/Community/controllers/posts.js
+++ SC2006-Backend/Community/controllers/posts.js
@@ -77,3 +77,25 @@ export const likePost = async (req, res) => {
     res.status(404).json({ message: err.message });
   }
 };
+
+/* COMMENT */
+
+export const commentPost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { value } = req.body; // value of comment
+
+    const post = await Post.findById(id);
+
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { comments: post.comments },
+      { new: true }
+    );
+
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
