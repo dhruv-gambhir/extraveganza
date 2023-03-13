7e47155224bf707e70d7b598c33125606be7f578
Enicul <93476156+Enicul@users.noreply.github.com>
2023-03-13 21:59:31 +0800

    Update posts.js

--- SC2006-Backend/Community/routes/posts.js
+++ SC2006-Backend/Community/routes/posts.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import { getFeedPosts, getUserPosts, likePost,commentPost } from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
@@ -11,5 +11,8 @@ router.get("/:userId/posts", verifyToken, getUserPosts);
 
 /* UPDATE */
 router.patch("/:id/like", verifyToken, likePost);
+
+/* COMMENT */
+router.post("/:id/comments",verifyToken,commentPost);
 
 export default router;
