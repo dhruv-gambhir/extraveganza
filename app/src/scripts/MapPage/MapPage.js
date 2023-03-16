280ae2687dfb6372b74bcea2696e85719ff736df
Hasterwfsc <90106147+Hasterwfsc@users.noreply.github.com>
2023-03-16 20:06:21 +0800

    Update MapPage.js

--- app/src/scripts/MapPage/MapPage.js
+++ app/src/scripts/MapPage/MapPage.js
@@ -16,9 +16,9 @@ export default class MapPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            lat: 1.3402,
-            lng: 103.6755,
-            address: 'Nanyang Technological University'
+            lat: 1.34640300173588,
+            lng: 103.686721630348,
+            address: 'Nanyang Technological University Hall 1'
         };
         this.handleCoordinatesChange = this.handleCoordinatesChange.bind(this);
         this.handleAddressChange = this.handleAddressChange.bind(this);
@@ -43,6 +43,9 @@ export default class MapPage extends Component {
                 <div className="main-content-container">
                     <Search onCoordinatesChange={this.handleCoordinatesChange} onAddressChange={this.handleAddressChange} />
                     {console.log(this.state.address)}
+                    {console.log(this.state.lat)}
+                    {console.log(this.state.lng)}
+
                     <SimpleMap center={{ lat: Number(this.state.lat), lng: Number(this.state.lng) }} address={this.state.address}/>
                 </div>
             </Fragment>
