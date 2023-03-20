85fc7b1c84e6a14ba97b45ed189d51c7826a9720
Enicul <93476156+Enicul@users.noreply.github.com>
2023-03-20 17:58:25 +0800

    Create auth.js

---
+++ Deliverables/BackendSkeleton/Community/Controllers/auth.js
@@ -0,0 +1,26 @@
+
+
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+/* REGISTER USER */
+export const register = async (req, res) => {
+
+
+};
+
+/* LOG IN */
+export const login = async (req, res) => {
+
+};
+
+/* UPDATE PASSWORD */
+export const update = async (req, res) => {
+
+};
+
+/* DELETE ACCOUNT*/
+export const deleteAccount = async (req, res) => {
+
+};
