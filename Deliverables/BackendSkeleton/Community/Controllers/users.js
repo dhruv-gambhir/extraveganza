1fa02866380fd6b2ea92fe1658b72439eb41660d
Enicul <93476156+Enicul@users.noreply.github.com>
2023-03-20 18:04:56 +0800

    Create users.js

---
+++ Deliverables/BackendSkeleton/Community/Controllers/users.js
@@ -0,0 +1,19 @@
+import User from "../models/User.js";
+
+/* READ USERS */
+export const getUser = async (req, res) => 
+{
+
+};
+
+/* GET USER'S FRIENDS */
+export const getUserFriends = async (req, res) => 
+{
+
+};
+
+/* ADD REMOVE FRIENDS */
+export const addRemoveFriend = async (req, res) => {
+
+
+};
