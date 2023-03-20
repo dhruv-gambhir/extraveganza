93afd6ce31245148d3d6550517d56fb9ce34d067
Enicul <93476156+Enicul@users.noreply.github.com>
2023-03-20 18:03:59 +0800

    Create posts.js

---
+++ Deliverables/BackendSkeleton/Community/Controllers/posts.js
@@ -0,0 +1,29 @@
+import Post from "../models/Post.js";
+import User from "../models/User.js";
+
+/* CREATE POSTS*/
+export const createPost = async (req, res) => 
+{
+
+};
+
+/* READ POST */
+export const getFeedPosts = async (req, res) => 
+
+};
+
+/* GET USER'S POSTS*/
+export const getUserPosts = async (req, res) => {
+
+};
+
+/* LIKE POST*/
+export const likePost = async (req, res) => {
+
+};
+
+/* COMMENT POSTS*/
+
+export const commentPost = async (req, res) => {
+
+};
