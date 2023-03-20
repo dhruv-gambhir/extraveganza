7633c824279c6c425a8b6f0c4511aba40747c0a9
Enicul <93476156+Enicul@users.noreply.github.com>
2023-03-20 17:41:38 +0800

    Update auth.js

--- SC2006-Backend/Community/routes/auth.js
+++ SC2006-Backend/Community/routes/auth.js
@@ -3,9 +3,16 @@ import { deleteAccount, login, register, update } from "../controllers/auth.js";
 
 const router = express.Router();
 
+/* LOGIN */
 router.post("/login", login);
+
+/* REGISTER */
 router.post("/register", register);
+
+/* UPDATE PASSWORD */
 router.post("/update", update);
+
+/* DELETE ACCOUNT */
 router.post("/deleteAccount", deleteAccount)
 
 export default router;
