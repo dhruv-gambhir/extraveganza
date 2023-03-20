12eab564531aedbe8d675976c6334e982d59a835
Enicul <93476156+Enicul@users.noreply.github.com>
2023-03-20 18:07:53 +0800

    Create posts.js

---
+++ Deliverables/BackendSkeleton/Community/Routes/posts.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { getFeedPosts, getUserPosts, likePost,commentPost } from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/", verifyToken, getFeedPosts);
+
+/* GRAB USER POSTS */
+router.get("/:userId/posts", verifyToken, getUserPosts);
+
+/* UPDATE */
+router.patch("/:id/like", verifyToken, likePost);
+
+/* COMMENT */
+router.post("/:id/comments",verifyToken,commentPost);
+
+export default router;
