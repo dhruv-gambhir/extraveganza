4559668ca15ae2bce5bf5af9fb2b7c5f8bfa2d0a
Bryan <bryanlu.my@gmail.com>
2023-03-23 15:10:31 +0800

    user can now like posts (no backend call)

--- app/src/scripts/CommunityPage/CommunityPage.js
+++ app/src/scripts/CommunityPage/CommunityPage.js
@@ -16,6 +16,10 @@ export default class CommunityPage extends Component {
 		this.listBuffer = [];
 	}
 
+	componentDidMount() {
+		this.retrieveCommunityPostsList();
+	}
+
 	/**
 	 * Retrieves the community posts
 	 * @date 3/15/2023 - 7:13:26 PM
@@ -25,23 +29,31 @@ export default class CommunityPage extends Component {
 	retrieveCommunityPostsList = () => {
 		// For now just return a const list
 		var communityPostList = [
-			{ id: 101010101, title: "Nice restaurant", restaurant: "i luv veggies", rating: 5, content: "So nice mmmmmm i like it " },
-			{ id: 1, title: "Title 1", restaurant: "Restaurant Name", rating: 5, content: "lorem ipsum dolor sit amet" },
-			{ id: 2, title: "Title 2", restaurant: "Restaurant Name", rating: 5, content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos, autem repellat, quaerat illum neque obcaecati nulla hic ipsam dolore eligendi minima asperiores repellendus perspiciatis velit ducimus cum quo culpa? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit inventore voluptas dicta ab odio accusantium accusamus. Ducimus tempore, cupiditate dignissimos, a nisi fugit quas molestiae consectetur accusantium perferendis magnam enim." },
-			{ id: 3, title: "Title 3", restaurant: "Restaurant Name", rating: 5, content: "lorem ipsum dolor sit amet" },
-			{ id: 4, title: "Title 4", restaurant: "Restaurant Name", rating: 5, content: "lorem ipsum dolor sit amet" },
+			{ id: 101010101, title: "Nice restaurant", restaurant: "i luv veggies", rating: 5, liked: true, likeCount: 69, content: "So nice mmmmmm i like it " },
+			{ id: 4, title: "Title 4", restaurant: "Restaurant Name", rating: 5, liked: false, likeCount: 69, content: "lorem ipsum dolor sit amet" },
+			{ id: 1, title: "Title 1", restaurant: "Restaurant Name", rating: 5, liked: true, likeCount: 69, content: "lorem ipsum dolor sit amet" },
+			{ id: 2, title: "Title 2", restaurant: "Restaurant Name", rating: 5, liked: true, likeCount: 69, content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos, autem repellat, quaerat illum neque obcaecati nulla hic ipsam dolore eligendi minima asperiores repellendus perspiciatis velit ducimus cum quo culpa? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit inventore voluptas dicta ab odio accusantium accusamus. Ducimus tempore, cupiditate dignissimos, a nisi fugit quas molestiae consectetur accusantium perferendis magnam enim." },
+			{ id: 3, title: "Title 3", restaurant: "Restaurant Name", rating: 5, liked: true, likeCount: 69, content: "lorem ipsum dolor sit amet" },
 		];
 		this.listBuffer = communityPostList;
 		return communityPostList;
 	};
 
-
 	filterRestaurantsBySearchQuery = () => {
 		var foo = this.listBuffer;
-		console.log(foo);
-		var foo = foo.filter((bar) => (bar.title.toLowerCase().includes(this.props.searchbarValue.toLowerCase()) || bar.restaurant.toLowerCase().includes(this.props.searchbarValue.toLowerCase()) || bar.content.toLowerCase().includes(this.props.searchbarValue.toLowerCase())));
-		this.listBuffer = foo;
+		foo = foo.filter((bar) => (bar.title.toLowerCase().includes(this.props.searchbarValue.toLowerCase()) || bar.restaurant.toLowerCase().includes(this.props.searchbarValue.toLowerCase()) || bar.content.toLowerCase().includes(this.props.searchbarValue.toLowerCase())));
+		// this.listBuffer = foo;
 		return foo;
+	};
+
+	toggleItemLike = (id) => {
+		const foo = this.listBuffer.findIndex(obj => obj.id === id);
+		if (foo !== -1) {
+			if (this.listBuffer[foo].liked) this.listBuffer[foo].likeCount--; else this.listBuffer[foo].likeCount++;
+			this.listBuffer[foo].liked = !this.listBuffer[foo].liked;
+			this.forceUpdate();
+			// Call function to update user's like post / post's like count
+		}
 	};
 
 	/**
@@ -51,15 +63,24 @@ export default class CommunityPage extends Component {
 	 * @returns {*}
 	 */
 	render() {
-		this.retrieveCommunityPostsList();
-		const list = this.filterRestaurantsBySearchQuery();
+		if (this.listBuffer.length === 0) {
+			this.retrieveCommunityPostsList();
+		}
+
+		var list = this.filterRestaurantsBySearchQuery();
 
 		return (
 			<Fragment>
 				{/* This will be generated automatically in the future, and updated when user scroll to the "end" of the list */}
 				<div className="main-content-container community-page-card-container">
 					{list.map((item) => (
-						<CommunityPost key={item.id}>
+						<CommunityPost
+							key={item.id}
+							itemID={item.id}
+							liked={item.liked}
+							likeCount={item.likeCount}
+							toggleItemLike={this.toggleItemLike}
+						>
 							<div>{item.title}</div>
 							<div>{item.restaurant}</div>
 							<div>{item.content}</div>
--- app/src/scripts/CommunityPage/CommunityPost.js
+++ app/src/scripts/CommunityPage/CommunityPost.js
@@ -9,7 +9,15 @@ import { Component } from "react";
  * @typedef {CommunityPost}
  * @extends {Component}
  */
-export default class CommunityPost extends Component {
+export default class CommunityPost extends Component {    
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextProps !== this.props) {
+            this.forceUpdate();
+            return true;
+        }
+        return false;
+    }
+
     /**
      * Renders the component's content
      * @date 3/13/2023 - 1:54:12 PM
@@ -18,7 +26,7 @@ export default class CommunityPost extends Component {
      */
     render() {
         return (
-            <div className="community-page-card">
+            <div className='community-page-card'>
                 <div className="community-page-card-text">
                     <div className="community-page-card-text-header">
                         {this.props.children[0]}
@@ -33,8 +41,12 @@ export default class CommunityPost extends Component {
 
                 <div className="community-page-card-bottom">
                     <div className="community-page-card-bottom-elem-container">
-                        <div className="community-page-card-bottom-elem">💖</div>
-                        <div className="community-page-card-bottom-elem">{Math.round(Math.random() * 69)}</div>
+                        <div
+                            className="community-page-card-bottom-elem community-page-card-bottom-elem-text"
+                            onClick={() => { this.props.toggleItemLike(this.props.itemID); }}>
+                            {this.props.liked ? '♥' : '♡'}
+                        </div>
+                        <div className="community-page-card-bottom-elem">{this.props.likeCount}</div>
 
                     </div>
                     {/* Comment section is disabled --> Find out why */}
