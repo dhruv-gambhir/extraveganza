13535c0550ae4e13eaca85d0df2522428b409cab
Enicul <93476156+Enicul@users.noreply.github.com>
2023-03-30 12:41:46 +0800

    add in restaurantID

--- SC2006-Backend-archive/Community/models/Post.js
+++ SC2006-Backend-archive/Community/models/Post.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
  * Schema of how posts are stored
  * @property {string} userID - user id of the owner of the post
  * @property {username} username - username of the owner of the post
+ * @property {mongoose.Schema.Types.ObjectId} restaurantID - the restaurant mentioned in the post
  * @property {string} description - content of post published by the post owner
  * @property {string} picturePath - address of image uploaded by post owner as part of the post
  * @property {string} userPicturePath - address of profile picture of post owner
@@ -19,6 +20,12 @@ const postSchema = mongoose.Schema(
     username:{
       type:String,
       required: true,
+    },
+    
+     restaurantID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Restaurant',
+      required: true
     },
 
     description: String,
